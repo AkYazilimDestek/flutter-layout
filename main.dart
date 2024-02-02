562a9f54a24ddfff1a4e66047881c3d07efcd278
AkYazilimDestek <111167457+AkYazilimDestek@users.noreply.github.com>
2024-02-03 01:19:41 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,6 +14,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -19,78 +23,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return MyLayout(
-      body: Center(
-        child: Text(
-          "Ana Sayfa",
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
+    return Layout(
+        title: 'Ana Sayfa',
+        body: Center(
+          child: Text(
+            "Ana Sayfa",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
-class MyLayout extends StatefulWidget {
-  const MyLayout({Key? key, required this.body}) : super(key: key);
-  final Widget body;
-
-  @override
-  State<MyLayout> createState() => _MyLayoutState();
-}
-
-class _MyLayoutState extends State<MyLayout> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Deneme Info",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 11, fontWeight: FontWeight.w300),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-        ],
-      ),
-      drawer: Drawer(),
-      body: widget.body,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Scaffold(
-                appBar: AppBar(
-                  title: Text("New Page"),
-                  leading: IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.pop(
-                          context); // Bu geri butonunun işlevini yerine getirir
-                    },
-                  ),
-                ),
-                body: Center(
-                  child: Text("Hello, this is a new page!"),
-                ),
-              ),
-            ),
-          );
-        },
-        child: Icon(Icons.alarm),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -102,17 +45,43 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return MyLayout(
-      body: Center(
-        child: Text(
-          "Hakkımızda Sayfa",
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
+    return Layout(
+        title: 'Hakkımızda Sayfası',
+        body: Center(
+          child: Text(
+            "Hakkımızda Sayfası",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
-        ),
+        ));
+  }
+}
+
+
+class Layout extends StatefulWidget {
+  const Layout({super.key, required this.body, required this.title});
+  final Widget body;
+  final String title;
+  @override
+  State<Layout> createState() => _LayoutState();
+}
+
+class _LayoutState extends State<Layout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+        ],
       ),
+      body: widget.body,
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {
+            Navigator.push(context,MaterialPageRoute(builder: (context) => AboutPage()));
+          }, child: Icon(Icons.add)),
     );
   }
 }
